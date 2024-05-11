36a983db7b925770ef583942af7cc88cf20a749f
Yesid-Ackerman~ <deironyesithym21@gmail.com>
2024-05-10 23:14:56 -0500

    TABLA ARTICULOS COMPLETA (1/4)

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function create(){
+
+        return view('articulos.article');
+
+    }
+
+    public function Store(Request $request){
+        $article = new article();
+        $article->nombre_articulo=$request->nombre_articulo;
+        $article->tipo_articulo=$request->tipo_articulo;
+        $article->costo=$request->costo;
+        $article->desc_art=$request->desc_art;
+        $article->save();
+        return $request;
+     }
+
+    public function index(){
+
+        $articles = article::orderBy('id', 'desc')->get();
+        return view('articulos/listar', compact('articles'));
+    }
+     public Function show (article $article){
+
+        return view('articulos/show', compact('article'));
+
+    }
+    public function destroy(article $article) {
+        $article->delete();
+        return redirect()->route('article.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+namespace App\Http\Controllers;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Articulos
+Route::get('articulos/listar',[ArticleController::class,'index'])->name('article.index');
+Route::get('articulos/article',[ArticleController::class,'create']);
+Route::post('/Store', [ArticleController::class,'Store'])->name('article.Store');
+Route::get('article/{article}',[ArticleController::class,'show'])->name('article.show');
+Route::delete('article/{article}',[ArticleController::class,'destroy'])->name('article.destroy');
