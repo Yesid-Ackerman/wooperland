a699562d6b24e8984663aa234270d922b063f762
HaiverVelasco <haivervelascos24@gma.com>
2024-06-05 12:43:16 -0500

    cambios llaves foraneas

---
+++ database/migrations/2024_08_05_215941_create_sugerencias_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sugerencias', function (Blueprint $table) {
+            $table->id();
+            $table->text('Tema_sugerencia');
+            $table->text('Actividad_sugerencia');
+            $table->text('Caracteristica_sugerencia');
+            $table->unsignedBigInteger('report_id')->nullable();
+            $table->foreign('report_id')->references('id')->on('reports')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sugerencias');
+    }
+};
